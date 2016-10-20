225e69b9a890e067c6b3d03996fa854cccbcdc96
LTR1 <LyndenRothfork@gmail.com>
2016-10-19 22:13:12 -0700

    Project 1 Version 2
    Masonry activated, some minor hiccups.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,8 @@ $('h1').lettering();
 // Make Backstretch Go!!
 $.backstretch('/img/bg.jpeg');
 // Activate Masonry
+$('.grid').masonry({
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 350
+});
