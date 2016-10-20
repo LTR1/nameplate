8b1d00e6c87d11a04f3c83feaea486533207ddeb
LTR1 <LyndenRothfork@gmail.com>
2016-10-19 20:10:15 -0700

    Nameplate Version 1
    Implemented JQuery plug-ins Flickity, Masonry, Lettering

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,5 @@
 // Intialize Lettering JS
 $('h1').lettering();
-
 // Make Backstretch Go!!
 $.backstretch('/img/bg.jpeg');
-
 // Activate Masonry
-
-$('.grid').masonry({
-  // options...
-  itemSelector: '.grid-item',
-  columnWidth: 200
-});
